04cbc960de564870088e4bf1778c95606bd0414e
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2020-01-14 16:31:54 +0700

    Define a sortable trait in the model file

--- app/Sharemarket.php
+++ app/Sharemarket.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Kyslik\ColumnSortable\Sortable;
 
 class Sharemarket extends Model
 {
-    //
+    use Sortable;
+
+    public $sortable = ['id','companyname','price','created_at','updated_at'];
 }
