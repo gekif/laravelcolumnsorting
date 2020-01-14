d05b181acf61ad03bf4dfe91e905b79fd31643c3
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2020-01-14 16:31:20 +0700

    Store data to the database

--- app/Http/Controllers/SharemarketController.php
+++ app/Http/Controllers/SharemarketController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Sharemarket;
 use Illuminate\Http\Request;
 
 class SharemarketController extends Controller
@@ -9,5 +10,17 @@ class SharemarketController extends Controller
     public function create()
     {
         return view('sharemarket');
+    }
+
+
+    public function store(Request $request)
+    {
+        $stock = new Sharemarket();
+
+        $stock->companyname = $request->get('companyname');
+        $stock->price = $request->get('price');
+        $stock->save();
+
+        return redirect('/sharemarket/index')->with('success', 'Stock has been added');
     }
 }
